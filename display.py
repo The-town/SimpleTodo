dc6caa6e47155b4abd75aa17b18ee400b27da43f
Matsukura <heuchannsight@yahoo.co.jp>
2021-12-11 11:53:03 +0900

    [修正]TODO詳細表示機能をdisplayクラスへ移した
    GUIのオブジェクトクラスの役割ではないため

--- display.py
+++ display.py
@@ -2,10 +2,14 @@ from tkinter import *
 from tkinter import messagebox
 from tkinter import filedialog, simpledialog
 import tkinter as tk
+from typing import Tuple
+
 from todo import Todo
-from gui_object import Frame, Label, Listbox, Text, Button, Combobox, DialogForAddTodo
+from gui_object import Frame, Label, Listbox, TextForDisplayDetail, Button, Combobox, DialogForAddTodo, CloseTodoButton
 from flatten import flatten
 import re
+import os
+import datetime
 
 
 class TodoDisplay:
@@ -24,7 +28,9 @@ class TodoDisplay:
         self.function_frame = Frame(self.root)
         self.function_frame.grid(column=0, columnspan=2, row=0)
 
-        self.listbox = Listbox(master=self.todo_list_frame, master_of_detail_text=self.todo_detail_frame)
+        self.todo_listbox = Listbox(master=self.todo_list_frame, master_of_detail_text=self.todo_detail_frame)
+        self.todo_listbox.bind("<Button-1>", self.display_todo_detail)
+        self.todo_listbox.bind("<Return>", self.display_todo_detail)
 
         self.add_todo_button = Button(master=self.function_frame)
         self.add_todo_button.grid(column=5, row=0, padx=5)
@@ -60,15 +66,89 @@ class TodoDisplay:
         for path in paths:
             todo_information = self.get_info_which_todo_have(path)
             contents_to_display = self.get_contents_to_display_which_todo_have(todo_information)
-            self.listbox.insert(todo_list_box_id, contents_to_display)
+            self.todo_listbox.insert(todo_list_box_id, contents_to_display)
 
             importance_color = self.todo.search_importance(todo_information["file_name"])
-            self.listbox.itemconfig(todo_list_box_id, {'bg': importance_color})
+            self.todo_listbox.itemconfig(todo_list_box_id, {'bg': importance_color})
 
             self.todo_list_box_dict[todo_list_box_id] = path
             todo_list_box_id = todo_list_box_id + 1
 
-        self.listbox.set_todo_list(self.todo_list_box_dict)
+        self.todo_listbox.set_todo_list(self.todo_list_box_dict)
+
+    def display_todo_detail(self, event=None) -> None:
+        if str(event.type) == "ButtonPress":
+            self.todo_listbox.activate_line_clicked(event=event)
+
+        self.todo_listbox.text.destroy()
+
+        todo_path: str = self.todo_listbox.todo_list[self.todo_listbox.index(ACTIVE)]
+        self.todo_listbox.text = TextForDisplayDetail(self.todo_listbox.master_of_detail_text)
+
+        self.todo_listbox.text.tag_bind("system_message_file_path", "<Double-Button-1>",
+                                        self.todo_listbox.open_with_another_app)
+        self.todo_listbox.text.tag_bind("system_message_folder_path", "<Double-Button-1>",
+                                        self.todo_listbox.open_folder)
+        create_time, update_time = self.get_todo_timestamp(todo_path)
+        self.todo_listbox.text.insert(END, "ファイルを開く", "system_message_file_path")
+        self.todo_listbox.text.insert(END, "\t\t")
+        self.todo_listbox.text.insert(END, "フォルダを開く", "system_message_folder_path")
+        self.todo_listbox.text.insert(END, "\n\n")
+        self.todo_listbox.text.insert(END, self.get_todo_detail(todo_path))
+
+        self.todo_listbox.text.insert(END, "\n\n======メタデータ======\n\n")
+        self.todo_listbox.text.insert(END, "作成 {0} 更新 {1}".format(create_time, update_time))
+        self.todo_listbox.text.insert(END, "\n")
+        self.todo_listbox.text.insert(END, todo_path)
+
+        self.todo_listbox.text.insert(END, "\n\n======TODO操作======\n\n")
+        close_todo_button = CloseTodoButton()
+        close_todo_button["command"] = self.todo_listbox.close_todo
+        self.todo_listbox.text.window_create(tk.END, window=close_todo_button)
+
+        self.todo_listbox.text.grid(column=0, row=1, columnspan=3)
+
+    @staticmethod
+    def get_todo_detail(path: str) -> str:
+        """
+        TODOファイルの詳細情報を取得するメソッド
+
+        Parameters
+        -----------
+        path: str
+            TODOファイルのパス
+
+        Returns
+        --------
+        f.read(): str
+            詳細情報
+        """
+        try:
+            with open(path, encoding="utf_8") as f:
+                return f.read()
+        except UnicodeDecodeError:
+            return "このファイルはプレビューできません。"
+
+    @staticmethod
+    def get_todo_timestamp(path: str) -> Tuple[str, str]:
+        """
+        TODOファイルが作成・更新されたタイムスタンプを取得するメソッド
+
+        Parameters
+        -----------
+        path: str
+            TODOファイルのパス
+
+        Returns
+        -------
+        create_time, update_time: Tuple[str, str]
+            作成日時と更新日時
+        """
+        stat_result = os.stat(path)
+        create_time = datetime.datetime.fromtimestamp(stat_result.st_ctime).strftime("%Y/%m/%d %H:%M:%S")
+        update_time = datetime.datetime.fromtimestamp(stat_result.st_mtime).strftime("%Y/%m/%d %H:%M:%S")
+
+        return create_time, update_time
 
     def get_paths_which_todo_file_have(self):
         paths = self.todo.get_paths_which_result_of_search(self.dir_combbox.get())
@@ -110,7 +190,7 @@ class TodoDisplay:
         return " ".join(flatten(content_list))
 
     def refresh(self, event=None):
-        self.listbox.delete(0, "end")
+        self.todo_listbox.delete(0, "end")
         self.display_todo()
 
     def add_todo(self, event=None):
--- gui_object.py
+++ gui_object.py
@@ -101,62 +101,14 @@ class Listbox(tk.Listbox):
         self.date_label = Label(self.master_of_detail_text)
 
         scrollbar["command"] = self.yview
-        self.bind("<Button-1>", self.show_detail)
-        self.bind("<Return>", self.show_detail)
 
         self.todo_list = {}
-
-    def show_detail(self, event=None):
-
-        if str(event.type) == "ButtonPress":
-            self.activate_line_clicked(event=event)
-
-        self.text.destroy()
-
-        self.text = TextForDisplayDetail(self.master_of_detail_text)
-
-        self.text.tag_bind("system_message_file_path", "<Double-Button-1>", self.open_with_another_app)
-        self.text.tag_bind("system_message_folder_path", "<Double-Button-1>", self.open_folder)
-        create_time, update_time = self.get_timestamp_of_path(self.todo_list[self.index(ACTIVE)])
-        self.text.insert(END, "ファイルを開く", "system_message_file_path")
-        self.text.insert(END, "\t\t")
-        self.text.insert(END, "フォルダを開く", "system_message_folder_path")
-        self.text.insert(END, "\n\n")
-        self.text.insert(END, self.read_detail_of_todo(self.index(ACTIVE)))
-
-        self.text.insert(END, "\n\n======メタデータ======\n\n")
-        self.text.insert(END, "作成 {0} 更新 {1}".format(create_time, update_time))
-        self.text.insert(END, "\n")
-        self.text.insert(END, self.todo_list[self.index(ACTIVE)])
-
-        self.text.insert(END, "\n\n======TODO操作======\n\n")
-        close_todo_button = CloseTodoButton()
-        close_todo_button["command"] = self.close_todo
-        self.text.window_create(tk.END, window=close_todo_button)
-
-        self.text.grid(column=0, row=1, columnspan=3)
 
     def set_todo_list(self, todo_list_dict):
         self.todo_list = todo_list_dict
 
     def get_todo_list(self):
         return self.todo_list
-
-    def read_detail_of_todo(self, index):
-        path = self.todo_list[index]
-        try:
-            with open(path, encoding="utf_8") as f:
-                return f.read()
-        except UnicodeDecodeError:
-            return "このファイルはプレビューできません。"
-
-    @staticmethod
-    def get_timestamp_of_path(path):
-        stat_result = os.stat(path)
-        create_time = datetime.datetime.fromtimestamp(stat_result.st_ctime).strftime("%Y/%m/%d %H:%M:%S")
-        update_time = datetime.datetime.fromtimestamp(stat_result.st_mtime).strftime("%Y/%m/%d %H:%M:%S")
-
-        return create_time, update_time
 
     def get_path_of_active_todo(self):
         return self.todo_list[self.index(ACTIVE)]
