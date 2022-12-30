1826c77d83b7181ad9cb77d616eeb4f871392c04
Matsukura <heuchannsight@yahoo.co.jp>
2022-12-30 19:16:59 +0900

    [更新]ダッシュボード機能を更新

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,5 +1,13 @@
+from typing import List, Tuple, Dict
+
 import abc
 import tkinter
+import tkinter.ttk as ttk
+
+
+class DashboardRootWindow:
+    def __init__(self):
+        self.root = tkinter.Tk()
 
 
 class ICounterForTodo(metaclass=abc.ABCMeta):
@@ -18,13 +26,22 @@ class ICounterForTodo(metaclass=abc.ABCMeta):
     def display(self) -> None:
         raise NotImplementedError()
 
+    @abc.abstractmethod
+    def create_label(self) -> tkinter.Label:
+        raise NotImplementedError()
+
 
 class CounterForActiveTodo(ICounterForTodo):
     """
     ダッシュボード上でアクティブなTODO数を表示する
     """
-    def __init__(self):
-        self.counter_var: tkinter.StringVar = tkinter.StringVar()
+    def __init__(self, dashboard_root: DashboardRootWindow, name: str, todos: tuple):
+        self.dashboard_root: DashboardRootWindow = dashboard_root
+        self.counter_var: tkinter.StringVar = tkinter.StringVar(master=self.dashboard_root.root)
+        self.todos: tuple = todos
+        self.name: str = name
+
+        self.display()
 
     def get_todo_state(self) -> tuple:
         """
@@ -34,8 +51,7 @@ class CounterForActiveTodo(ICounterForTodo):
         --------
         todos: tuple
         """
-        todos: tuple = ("active", "no_active", "active")
-        return todos
+        return self.todos
 
     def sum_number_of_todo(self) -> int:
         """
@@ -45,8 +61,8 @@ class CounterForActiveTodo(ICounterForTodo):
         --------
         number_of_active: int
         """
-        todos = self.get_todo_state()
-        number_of_active: int = len([todo for todo in todos if todo == "active"])
+        todos: tuple = self.get_todo_state()
+        number_of_active: int = len(todos)
         return number_of_active
 
     def display(self) -> None:
@@ -58,16 +74,37 @@ class CounterForActiveTodo(ICounterForTodo):
         None
         """
         number_of_active: int = self.sum_number_of_todo()
-        self.counter_var.set(str(number_of_active))
+        self.counter_var.set(": ".join([self.name, str(number_of_active)]))
+
+    def create_label(self) -> tkinter.Label:
+        """
+        ダッシュボード用にラベルを作るメソッド
+
+        Returns
+        -------
+        counter_label: tkinter.Label
+        """
+        counter_label: ttk.Label = ttk.Label(self.dashboard_root.root)
+        counter_label["padding"] = "1c"
+        counter_label["font"] = ("メイリオ", 26)
+        counter_label["background"] = "green"
+        counter_label["foreground"] = "white"
+        counter_label["relief"] = "groove"
+        counter_label["textvariable"] = self.counter_var
+
+        return counter_label
 
 
 class DashBoard:
     """
     ダッシュボードを表すクラス
     """
-    def __init__(self, name="ダッシュボード") -> None:
-        self.root = tkinter.Tk()
+    def __init__(self, dashboard_root: DashboardRootWindow, name="ダッシュボード") -> None:
+        super().__init__()
+        self.root: tkinter.Tk = dashboard_root.root
         self.root.title(name)
+
+        self.counters: Dict[str: ICounterForTodo, ...] = {}
 
     def display(self) -> None:
         """
@@ -77,16 +114,16 @@ class DashBoard:
         -------
         None
         """
-        counter_active_todo: CounterForActiveTodo = CounterForActiveTodo()
-        counter_active_todo.display()
-
-        active_todo_label: tkinter.Label = tkinter.Label(self.root)
-        active_todo_label["textvariable"] = counter_active_todo.counter_var
-        active_todo_label.grid(column=0, row=0)
+        for row, counter in enumerate(self.counters.values()):
+            self.create_counter_for_active_todo(counter, column=0, row=row)
 
         self.root.mainloop()
 
+    def add_counter(self, name: str, counter: ICounterForTodo):
+        self.counters[name] = counter
 
-if __name__ == "__main__":
-    dashboard = DashBoard()
-    dashboard.display()
+    @staticmethod
+    def create_counter_for_active_todo(counter: ICounterForTodo, column: int, row: int):
+        counter_label: tkinter.Label = counter.create_label()
+        counter_label.grid(column=column, row=row)
+
